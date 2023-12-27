e174c31ca15d020dd4341bab5b9cf533df41d35f
wwwkr <xodn1003@naver.com>
2023-12-27 14:36:51 +0900

    테스트 코드 추가

--- domain/src/test/java/com/wwwkr/domain/UseCaseTest.kt
+++ domain/src/test/java/com/wwwkr/domain/UseCaseTest.kt
@@ -10,6 +10,7 @@ import com.wwwkr.domain.usecase.LoginUseCase
 import com.wwwkr.domain.usecase.NewsUseCase
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.core.test.TestCase
+import io.kotest.matchers.comparables.shouldBeGreaterThan
 import io.kotest.matchers.shouldBe
 import io.mockk.coEvery
 import io.mockk.every
@@ -30,13 +31,13 @@ class UseCaseTest : ShouldSpec({
     val fakeNewsResponse = NewsRespData(
         articles = listOf<ArticleData>(),
         status = "00",
-        totalResults = 0
+        totalResults = 10
     )
 
     every { newsUseCase.getNews(country = "kr") } returns flowOf(fakeNewsResponse)
 
     // 테스트 케이스
-    context("API가 정상적으로 호출 했을 때") {
+    context("뉴스 API 호출") {
 
         // Flow를 collect하여 값을 처리
         val result = mutableListOf<NewsRespData>()
@@ -47,6 +48,10 @@ class UseCaseTest : ShouldSpec({
         should("status 는 00을 반환 한다") {
             // 마지막으로 방출된 값의 status가 "00"인지 확인
             result.lastOrNull()?.status shouldBe "00"
+        }
+
+        should("totalResults 는 0보다 커야 한다") {
+            result.lastOrNull()?.totalResults?.shouldBeGreaterThan(0)
         }
     }
 })
